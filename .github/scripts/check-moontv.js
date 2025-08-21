d0f5b0c92dfae98fefd7014908ac359cff6bac69
zhanjun520 <hggfkgh@gmail.com>
2025-08-21 16:12:15 +0800

    Update check-moontv.js

--- .github/scripts/check-moontv.js
+++ .github/scripts/check-moontv.js
@@ -23,12 +23,13 @@ async function checkApi(key) {
   for (let attempt = 1; attempt <= RETRIES; attempt++) {
     try {
       const res = await axios.get(url, { timeout: TIMEOUT,
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Language': 'zh-CN,zh;q=0.9',
-        // 'Referer': referer, // 一些站点需要 referer
-      }});
+      // headers: {
+      //   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36',
+      //   'Accept': 'application/json, text/javascript, */*; q=0.01',
+      //   'Accept-Language': 'zh-CN,zh;q=0.9',
+      //   // 'Referer': referer, // 一些站点需要 referer
+      //   }
+      });
       if (typeof res.data === 'object') {
         delete apiEntry.disabled;
         return { key, status: 'OK' };
